98a73315f5e51244c532f6fa1d775f6eafe11e53
jjjbenderjjj <jjjbenderjjj@gmail.com>
2016-11-07 14:38:39 +0200

    002

--- E-olimp.pro
+++ E-olimp.pro
@@ -3,7 +3,11 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
-SOURCES += main.cpp
+INCLUDEPATH += \
+    /home/bender/prevHome/home_6_8_2015/samples/hello-jni_____copy/jni
+
+SOURCES += main.cpp \
+    ../jni/Vector_picture_static.cpp
 
 DISTFILES += \
     in.txt
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <pwd.h>
 #include <unistd.h>
+#include <Vector_picture_static.h>
+
 
 using namespace std;
 
@@ -9,18 +11,38 @@ static const int N = 20;
 
 int main(int argc, char *argv[])
 {
+    // Get the last position of '/'
+    std::string aux(argv[0]);
 
-    //struct passwd *pw = getpwuid(getuid());
-    std::string directory = /*std::string(pw->pw_dir) + std::string("/in.txt")*/
-            "/home/bender/E-olimp/in.txt";
+    // get '/' or '\\' depending on unix/mac or windows.
+#if defined(_WIN32) || defined(WIN32)
+    int pos = aux.rfind('\\');
+#else
+    int pos = aux.rfind('/');
+#endif
 
-    ifstream in(directory);
+    // Get the path and the name
+    std::string path = aux.substr(0,pos+1);
+    std::string name = aux.substr(pos+1);
+    // show results
+
+    path += std::string("in.txt");
+
+    ifstream in(path);
     cin.rdbuf(in.rdbuf());
 
-    int j, i, q, p, id[N];
+    int j, i, q, p, id[N], sz[N];
 
     for (i = 0; i < N; ++i)
         id[i] = i;
+
+    __masI::LOG_mas(id,N,1);
+    new_line;
+    new_line;
+
+    for (i = 0; i < N; ++i)
+        sz[i] = 1;
+
 
     while(cin >> p >> q)
     {
@@ -33,28 +55,64 @@ int main(int argc, char *argv[])
             if(id[i] == t)
                 id[i] = id[q];
 
+        __masI::LOG_mas(id,N,1);
+        continue;
+#       endif
+
+#       if 0
+
+        int tm0 = p;
+        for (; p != id[p];)
+        {
+            p = id[p];
+        }
+
+
+        int tm1 = q;
+        for (; q != id[q]; )
+        {
+            q = id[q];
+        }
+
+
+        if(p == q) continue;
+
+        id[p] = q;
+
+        __masI::LOG_mas(id,N,1);
         continue;
 #       endif
 
 #       if 1
 
-        i = p;
-        for (; i != id[i];)
+        for (; p != id[p];)
         {
-            i = id[i];
+            p = id[id[p]];
         }
 
-        j = q;
-        for (; j != id[j]; )
+        for (; q != id[q]; )
         {
-            j = id[j];
+            q = id[id[q]];
         }
 
-        if(i == j) continue;
 
-        id[i] = j;
+        if(p == q) continue;
 
+        if (sz[p] <= sz[q])
+        {
+            id[p] = q;
+            sz[q] += sz[p];
+        }
+        else
+        {
+            id[q] = p;
+            sz[p] += sz[q];
+        }
+
+        __masI::LOG_mas(id,N,1);
+        continue;
 #       endif
-        cout << p << " " << q << endl;
+
+
     }
 }
